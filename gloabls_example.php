14d43286a8189f8832997a85b799c6cb1cbe76b7
Eric <errkkgeorge@gmail.com>
2011-08-16 10:40:38 +0100

    added functions to nav sec class so it can show child pages for the top level section that is assocaited with post types, so the single post type pages can be have the correct active nav state for their parent listing page

---
+++ gloabls_example.php
@@ -0,0 +1,14 @@
+
+// Page IDs of the listing pages for post types
+$GLOBALS['post_type_listing_page_ids'] = array(
+  'news'    => 50,
+  'events'  => 46,
+  'research'=> 252
+);
+
+// Post type listing page, top level parent pages, to spoof to navsec to show the full nav for that section
+$GLOBALS['post_type_listing_page_top_level_parent'] = array(
+  'news'    => 40,
+  'events'  => 40,
+  'research'=> 26
+);
--- nav-sec.class.php
+++ nav-sec.class.php
@@ -1,5 +1,4 @@
 
-
 class NavSec
 {
     private $current_post = 0;
@@ -25,11 +24,24 @@ class NavSec
      */
     function get_top_level_ancestor( $post_id )
     {
-	$tmp = get_page( $id );
-	while ( intVal( $tmp->post_parent ) > 0 ) {
-	    $tmp = get_page( $tmp->post_parent );
+	// Pages are hiererarchical and have ancestors, find the top level one
+	if( is_page() ){
+	    $tmp = get_page( $id );
+	    while ( intVal( $tmp->post_parent ) > 0 ) {
+		$tmp = get_page( $tmp->post_parent );
+	    }
+	    return $tmp;
+	    
+	}else{
+	    
+	    // If its a custom post type single page, see if its got a top level parent page associated with it in globals
+	    $post_type = get_query_var( 'post_type' );
+	    if( $GLOBALS['post_type_listing_page_top_level_parent'][$post_type] ){
+		$tmp = get_post( $GLOBALS['post_type_listing_page_top_level_parent'][$post_type] );
+		return $tmp;
+	    }
+	    
 	}
-	return $tmp;
     }
     
     /**
@@ -75,7 +87,7 @@ class NavSec
         
     /**
      * Loop thru current level of pages. For the current item, check if there are sub pages, and call this function again inside to do that level
-     * @param Object $page
+     * @param int $page
      * @return bool 
      */
     function level( $page )
@@ -95,9 +107,13 @@ class NavSec
 	$i = 1;
 	foreach( $pages as $item ){
 	    $class = '';
+	    
+	    // Last Class
 	    if( $count_pages == $i ){
-		$class .= 'last'  ;
+		$class .= 'last';
 	    }
+	    
+	    // Current Item (hierarchical) could have subnav
 	    if ( $this->is_current( $item->ID ) ) {
 		$class .= ' ' . $this->current_class;
 		// Current second level page
@@ -110,8 +126,18 @@ class NavSec
 		// Check for Sub Pages, and display sub list
 		$this->level( $item->ID );
 		
-
 		$this->output_lines[] = '</li>';
+	    
+		
+	    }elseif( $this->check_post_type_parent( $item->ID ) ){
+		// Current item - post type listing page, so appears as parent
+		$class .= ' ' . $this->current_class;
+		$this->output_lines[] = sprintf( 
+		    '<li class="%s"><a href="%s">%s</a></li>', 
+			$class, 
+			get_permalink( $item->ID ), 
+			$item->post_title 
+		);
 	    }else{
 		// normal list items, not current, no subnav
 		$this->output_lines[] = sprintf( '<li class="%s"><a href="%s">%s</a></li>', $class, get_permalink( $item->ID ), $item->post_title );
@@ -123,6 +149,22 @@ class NavSec
 	
 	$this->depth ++;
 	return true;
+    }
+    
+    /**
+     * Check current post type against $GLOBALS['post_type_listing_page_ids'] defined in globals.php
+     * to see if the current page (in the nav menu loop) is the listing page for this post type.
+     * @param int $parent_page_id id of the page in the nav that might be current
+     * @return bool
+     */
+    function check_post_type_parent( $parent_page_id )
+    { 
+	$post_type = get_query_var( 'post_type' );
+	if( 'page' !== $post_type 
+		&& array_key_exists( $post_type, $GLOBALS['post_type_listing_page_ids'] )
+		&& $parent_page_id === $GLOBALS['post_type_listing_page_ids'][$post_type] ){
+	    return true;
+	}
     }
     
     function get_ul_id( $level )
